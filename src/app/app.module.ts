3ec9c88c9fcd5f00c3118a42960393b6e178e95f
Jason Edstrom <jason@instacode.io>
2023-06-08 19:19:16 -0500

    changed the app file locations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CoursesModule } from './courses/courses.module';
+import { CoursesModule } from '../courses/courses.module';
 
 @Module({
   imports: [
